ad2a83928062adc30e155faffd5ced971275547c
Estevan Chavira <schavira@bearcubs.santarosa.edu>
2025-10-03 12:10:36 -0700

    add authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,77 +1,92 @@
-"use client";
-import React, { useEffect } from "react";
-import Link from "next/link";
+"use client"; // Use the client
+import React, { useEffect } from "react"; // Import the React and useEffect functions
+import Link from "next/link"; // Import the Link function
 import {
-  signInWithGoogle,
-  signOut,
-  onIdTokenChanged,
-} from "@/src/lib/firebase/auth.js";
-import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js";
-import { setCookie, deleteCookie } from "cookies-next";
+  signInWithGoogle, // Import the signInWithGoogle function
+  signOut, // Import the signOut function
+  onIdTokenChanged, // Import the onIdTokenChanged function
+} from "@/src/lib/firebase/auth.js"; // Import the signInWithGoogle, signOut, and onIdTokenChanged functions
+import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js"; // Import the addFakeRestaurantsAndReviews function
+import { setCookie, deleteCookie } from "cookies-next"; // Import the setCookie and deleteCookie functions
 
-function useUserSession(initialUser) {
-  return;
-}
+function useUserSession(initialUser) { // Export the useUserSession function
+  useEffect(() => { // Use the useEffect hook
+    return onIdTokenChanged(async (user) => { // Return the onIdTokenChanged function
+      if (user) { // If the user is authenticated
+        const idToken = await user.getIdToken(); // Get the ID token
+        await setCookie("__session", idToken); // Set the cookie
+      } else { // If the user is not authenticated
+        await deleteCookie("__session"); // Delete the cookie
+      } // End of the if statement
+      if (initialUser?.uid === user?.uid) { // If the user is the same
+        return; // Return if the user is the same
+      } // End of the if statement
+      window.location.reload(); // Reload the page
+    }); // End of the onIdTokenChanged function
+  }, [initialUser]); // Use the useEffect hook
 
-export default function Header({ initialUser }) {
-  const user = useUserSession(initialUser);
+  return initialUser; // Return the initialUser
+} // End of the useUserSession function
 
-  const handleSignOut = (event) => {
-    event.preventDefault();
-    signOut();
-  };
+export default function Header({ initialUser }) { // Export the Header function
+  const user = useUserSession(initialUser); // Use the useUserSession function
 
-  const handleSignIn = (event) => {
-    event.preventDefault();
-    signInWithGoogle();
-  };
+  const handleSignOut = (event) => { // Export the handleSignOut function
+    event.preventDefault(); // Prevent the default event
+    signOut(); // Sign out the user
+  }; // End of the handleSignOut function
 
-  return (
-    <header>
-      <Link href="/" className="logo">
-        <img src="/friendly-eats.svg" alt="FriendlyEats" />
+  const handleSignIn = (event) => { // Export the handleSignIn function
+    event.preventDefault(); // Prevent the default event
+    signInWithGoogle(); // Sign in with the GoogleAuthProvider object
+  }; // End of the handleSignIn function
+
+  return ( // Return the header
+    <header> // End of the handleSignOut function
+      <Link href="/" className="logo"> // End of the handleSignOut function
+        <img src="/friendly-eats.svg" alt="FriendlyEats" /> // End of the handleSignOut function
         Friendly Eats
-      </Link>
-      {user ? (
-        <>
-          <div className="profile">
-            <p>
+      </Link> // End of the handleSignOut function
+      {user ? ( // If the user is authenticated
+        <> // End of the handleSignOut function
+          <div className="profile"> // End of the handleSignOut function
+            <p> // End of the handleSignOut function
               <img
                 className="profileImage"
                 src={user.photoURL || "/profile.svg"}
                 alt={user.email}
-              />
+              /> // End of the handleSignOut function
               {user.displayName}
-            </p>
+            </p> // End of the handleSignOut function
 
-            <div className="menu">
-              ...
-              <ul>
-                <li>{user.displayName}</li>
+            <div className="menu"> // End of the handleSignOut function
+              ... // End of the handleSignOut function
+              <ul> // End of the handleSignOut function
+                <li>{user.displayName}</li> // End of the handleSignOut function
 
-                <li>
-                  <a href="#" onClick={addFakeRestaurantsAndReviews}>
-                    Add sample restaurants
-                  </a>
-                </li>
+                <li> // End of the handleSignOut function
+                  <a href="#" onClick={addFakeRestaurantsAndReviews}> // Add sample restaurants
+                    Add sample restaurants // End of the handleSignOut function
+                  </a> // End of the handleSignOut function
+                </li> // End of the handleSignOut function
 
-                <li>
-                  <a href="#" onClick={handleSignOut}>
-                    Sign Out
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="profile">
-          <a href="#" onClick={handleSignIn}>
-            <img src="/profile.svg" alt="A placeholder user image" />
-            Sign In with Google
-          </a>
-        </div>
-      )}
-    </header>
-  );
-}
+                <li> // End of the handleSignOut function
+                  <a href="#" onClick={handleSignOut}> // Sign out the user
+                    Sign Out // End of the handleSignOut function
+                  </a> // End of the handleSignOut function
+                </li> // End of the handleSignOut function
+              </ul> // End of the handleSignOut function
+            </div> // End of the handleSignOut function
+          </div> // End of the handleSignOut function
+        </> // End of the handleSignOut function
+      ) : ( //  If the user is not authenticated
+        <div className="profile"> // End of the handleSignIn function
+          <a href="#" onClick={handleSignIn}> // Sign in with the GoogleAuthProvider object
+            <img src="/profile.svg" alt="A placeholder user image" /> // End of the handleSignIn function
+            Sign In with Google // End of the handleSignIn function
+          </a> // End of the handleSignIn function
+        </div> // End of the handleSignIn function
+      )} // End of the handleSignIn function
+    </header> // End of the header
+  ); // End of the header
+} // End of the Header function
--- src/lib/firebase/serverApp.js
+++ src/lib/firebase/serverApp.js
@@ -1,14 +1,30 @@
 // enforces that this code can only be called on the server
 // https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#keeping-server-only-code-out-of-the-client-environment
-import "server-only";
+import "server-only"; // Import the server-only module
 
-import { cookies } from "next/headers";
-import { initializeServerApp, initializeApp } from "firebase/app";
+import { cookies } from "next/headers"; // Import the cookies function
+import { initializeServerApp, initializeApp } from "firebase/app"; // Import the initializeServerApp and initializeApp functions
 
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"; // Import the getAuth function
 
 // Returns an authenticated client SDK instance for use in Server Side Rendering
 // and Static Site Generation
-export async function getAuthenticatedAppForUser() {
-  throw new Error("not implemented");
-}
+export async function getAuthenticatedAppForUser() { // Export the getAuthenticatedAppForUser function
+  const authIdToken = (await cookies()).get("__session")?.value; // End of the cookies function
+
+  // Firebase Server App is a new feature in the JS SDK that allows you to
+  // instantiate the SDK with credentials retrieved from the client & has
+  // other affordances for use in server environments.
+  const firebaseServerApp = initializeServerApp( // End of the initializeServerApp function
+    // https://github.com/firebase/firebase-js-sdk/issues/8863#issuecomment-2751401913
+    initializeApp(), // End of the initializeApp function
+    { // End of the initializeServerApp function
+      authIdToken, // End of the authIdToken function
+    } // End of the initializeServerApp function  
+  ); // End of the initializeServerApp function
+
+  const auth = getAuth(firebaseServerApp); // End of the getAuth function
+  await auth.authStateReady(); // End of the authStateReady function
+
+  return { firebaseServerApp, currentUser: auth.currentUser }; // End of the getAuthenticatedAppForUser function
+} // End of the getAuthenticatedAppForUser function
